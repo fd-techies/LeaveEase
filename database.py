a9551927bc11b406d5c75486709877b3109c0a83
fd-techies <fd.techies@gmail.com>
2023-07-27 20:21:04 +0800

    FIX: connecting streamlit to deta

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 
 # Load the environment variables
-load_dotenv(".env")
-DETA_KEY = os.getenv("DETA_KEY")
-# DETA_KEY = st.secrets["DETA_KEY"]
+# load_dotenv(".env")
+# DETA_KEY = os.getenv("DETA_KEY")
+DETA_KEY = st.secrets["DETA_KEY"]
 
 # Initialize with a project key
 deta = Deta(DETA_KEY)
