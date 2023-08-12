5821abdaf6c833f996821bc1390bfbede863691e
fd-techies <fd.techies@gmail.com>
2023-08-12 11:25:04 +0800

    test: adding username and password to database

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+#https://leaveease.streamlit.app/
 import calendar  # Core Python Module
 from datetime import datetime, date  # Core Python Module
 
 import streamlit as st  # pip install streamlit
 from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 
+from database import Users
+
 # -------------- SETTINGS --------------
-page_title = "Income and Expense Tracker"
+page_title = "LeaveEase"
 page_icon = ":luggage:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "wide"
 # --------------------------------------
@@ -29,13 +32,22 @@ months = list(calendar.month_name[1:])
 if selected == "Leave Application":
     with st.form("leave_application", clear_on_submit=True):
         col1, col2 = st.columns(2)
-        col1.date_input("Start Date:", date(1970, 1, 1), min_value=date(1970, 1, 1),key="startdate")
-        col2.date_input("End Date:", date(1970, 1, 1), min_value=date(1970, 1, 1),key="enddate")
+        col1.text_input(label="Create new username", key="username")
+        col2.text_input(label="Create new password (do not user personal sensitive password)", key="password")
+        # col1.date_input("Start Date:", date(1970, 1, 1), min_value=date(1970, 1, 1),key="startdate")
+        # col2.date_input("End Date:", date(1970, 1, 1), min_value=date(1970, 1, 1),key="enddate")
 
         submitted = st.form_submit_button("Save Data")
         if submitted:
-            period = str(st.session_state["startdate"]) + "_" + str(st.session_state["enddate"])
-            st.success("Leave Applied from" + period)
+            users = Users(local=False)
+            newUsername = str(st.session_state["username"])
+            newPassword = str(st.session_state["password"])
+            try:
+                users.addUser(username = newUsername, password = newPassword)
+                st.success("Account has been successfuly created")
+            except:
+                errorMessage = RuntimeError('This is an exception of type RuntimeError')
+                st.exception(errorMessage)
 
 # --- INPUT & SAVE PERIODS (Leave Deletion) ---
 if selected == "Leave Deletion":
--- database.py
+++ database.py
@@ -4,21 +4,22 @@ import streamlit as st  # pip install streamlit
 from deta import Deta  # pip install deta
 from dotenv import load_dotenv
 
+class Users:
 
-# Load the environment variables
-# load_dotenv(".env")
-# DETA_KEY = os.getenv("DETA_KEY")
-DETA_KEY = st.secrets["DETA_KEY"]
+    def __init__(self, local: bool):
+        self.local = local
+        if self.local:
+           load_dotenv(".env")
+           self.deta = Deta(os.getenv("DETA_KEY"))
+        else:
+           self.deta = Deta(st.secrets["DETA_KEY"])
+        
 
-# Initialize with a project key
-deta = Deta(DETA_KEY)
-
-# This is how to create/connect a database
-db = deta.Base("user_leave_period")
-
-def insert_period(name: str, start_date: str, end_date: str):
-    """Returns the report on a successful creation, otherwise raises an error"""
-    return db.put({"key": name, "start_date": start_date, "end_date": end_date})
-
+    def addUser(self, username: str, password: str):
+        #     """Returns the report on a successful creation, otherwise raises an error"""
+        db = self.deta.Base("users")
+        return db.put({"username": username, "password": password})
+    
 if __name__=="__main__":
-    insert_period("Elwin","121223","131223")
+    users = Users(local=True)
+    users.addUser(username = "Elwin", password = "1234")
